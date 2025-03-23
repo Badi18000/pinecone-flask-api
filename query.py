2d2456c9783d62ce7891572553bb5086ccdbee74
Badi18000 <oubadi.mohamed@gmail.com>
2025-03-23 13:29:27 -0400

    Update query.py

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-from pinecone import Pinecone
+from pinecone
 import spacy
 import os
 from dotenv import load_dotenv
@@ -10,8 +10,10 @@ PINECONE_ENV = os.getenv("PINECONE_ENV")
 INDEX_NAME = os.getenv("INDEX_NAME")
 DIMENSION = 300
 
-pc = Pinecone(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
-index = pc.Index(INDEX_NAME)
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+index = pinecone.Index(INDEX_NAME)
+
+
 nlp = spacy.load("fr_core_news_md")
 
 def query_pinecone(user_query, top_k=5):
