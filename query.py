2c4e25d4c68dd991a0aa04d5e84774d1e8c1a1a7
Badi18000 <oubadi.mohamed@gmail.com>
2025-03-23 13:41:26 -0400

    Update query.py

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-import pinecone
+from pinecone import Pinecone, ServerlessSpec
 import spacy
 import os
 from dotenv import load_dotenv
@@ -10,8 +10,17 @@ PINECONE_ENV = os.getenv("PINECONE_ENV")
 INDEX_NAME = os.getenv("INDEX_NAME")
 DIMENSION = 300
 
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
-index = pinecone.Index(INDEX_NAME)
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+if INDEX_NAME not in pc.list_indexes().names():
+    pc.create_index(
+        name=INDEX_NAME,
+        dimension=DIMENSION,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+
+index = pc.Index(INDEX_NAME
 
 
 nlp = spacy.load("fr_core_news_md")
